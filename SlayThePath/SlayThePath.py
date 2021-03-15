b350b28ea8aa35a40569f62593eb9ee0a33b6933
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2021-03-15 10:55:51 +0100

    added more test code

--- SlayThePath/SlayThePath.py
+++ SlayThePath/SlayThePath.py
@@ -1,1 +1,22 @@
+import tensorflow as tf
 
+mnist = tf.keras.datasets.mnist
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(3),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
